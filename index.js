4068434ab1bffdc1ae1d97330c663e5803816427
ayeasin14 <ayeasin14@gmail.com>
2022-11-03 19:43:38 +0600

    some changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,12 +15,44 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 
+async function run() {
+
+    try {
+        const productCollection = client.db('emaJohn').collection('products')
+
+        app.get('/products', async (req, res) => {
+
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+            console.log(page, size);
+
+            const query = {}
+            const cursor = productCollection.find(query);
+            const products = await cursor.skip(page * size).limit(size).toArray();
+            const count = await productCollection.estimatedDocumentCount();
+            res.send({ count, products });
+        });
+
+        app.post('/productsByIds', async (req, res) => {
+            const ids = req.body;
+            const objectIds = ids.map(id => ObjectId(id))
+            const query = { _id: { $in: objectIds } };
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+
+    }
+    finally {
+
+    }
+
+}
+run().catch(err => console.error(err));
 
 app.get('/', (req, res) => {
     res.send('ema john server is running');
 });
-
-
 
 
 
